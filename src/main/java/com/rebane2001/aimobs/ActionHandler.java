1f551a1d7e8f3eeb371f8ed59922e48c1cedb7e4
Tony Stark <alex99ata@gmail.com>
2023-05-26 21:59:59 +0200

    entityBaseName and entityDisplayName variables

--- src/main/java/com/rebane2001/aimobs/ActionHandler.java
+++ src/main/java/com/rebane2001/aimobs/ActionHandler.java
@@ -16,7 +16,6 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Util;
 import net.minecraft.registry.RegistryKey;
-// import net.minecraft.world.EntityView;
 import net.minecraft.world.biome.Biome;
 import org.apache.commons.lang3.StringUtils;
 
@@ -28,11 +27,12 @@ import java.util.UUID;
 public class ActionHandler {
     public static String prompts = "";
     public static String entityName = "";
+    public static String entityBaseName = ""; // added new variable for base name
+    public static String entityDisplayName = ""; // added new variable for display name
     public static int entityId = 0;
     public static UUID initiator = null;
     public static long lastRequest = 0;
 
-    // The waitMessage is the thing that goes '<Name> ...' before an actual response is received
     private static ChatHudLine.Visible waitMessage;
     private static List<ChatHudLine.Visible> getChatHudMessages() {
         return ((ChatHudAccessor)MinecraftClient.getInstance().inGameHud.getChatHud()).getVisibleMessages();
@@ -60,12 +60,11 @@ public class ActionHandler {
         ItemStack heldItem = player.getMainHandStack();
         if (heldItem.getCount() > 0)
             prompts = "You are holding a " + heldItem.getName().getString() + " in your hand. " + prompts;
-        showWaitMessage(entityName);
+        showWaitMessage(entityDisplayName); // use display name here
         getResponse(player);
     }
 
     public static void getResponse(PlayerEntity player) {
-        // 1.5 second cooldown between requests
         if (lastRequest + 1500L > System.currentTimeMillis()) return;
         if (AIMobsConfig.config.apiKey.length() == 0) {
             player.sendMessage(Text.of("[AIMobs] You have not set an API key! Get one from https://beta.openai.com/account/api-keys and set it with /aimobs setkey"));
@@ -75,7 +74,7 @@ public class ActionHandler {
         Thread t = new Thread(() -> {
             try {
                 String response = RequestHandler.getAIResponse(prompts);
-                player.sendMessage(Text.of("<" + entityName + "> " + response));
+                player.sendMessage(Text.of("<" + entityDisplayName + "> " + response)); // use display name here
                 prompts += response + "\"\n";
             } catch (Exception e) {
                 player.sendMessage(Text.of("[AIMobs] Error getting response"));
@@ -90,7 +89,7 @@ public class ActionHandler {
     public static void replyToEntity(String message, PlayerEntity player) {
         if (entityId == 0) return;
         prompts += (player.getUuid() == initiator) ? "You say: \"" : ("Your friend " + player.getName().getString() + " says: \"");
-        prompts += message.replace("\"", "'") + "\"\n The " + entityName + " says: \"";
+        prompts += message.replace("\"", "'") + "\"\n The " + entityDisplayName + " says: \""; // use display name here
         getResponse(player);
     }
 
@@ -100,46 +99,50 @@ public class ActionHandler {
 
     private static String createPromptVillager(VillagerEntity villager, PlayerEntity player) {
         boolean isHurt = isEntityHurt(villager);
-        entityName = "Villager";
+        entityBaseName = "Villager";
+        entityDisplayName = entityBaseName; // initially set display name to be the base name
         String villageName = villager.getVillagerData().getType().toString().toLowerCase(Locale.ROOT) + " village";
         int rep = villager.getReputation(player);
         if (rep < -5) villageName = villageName + " that sees you as horrible";
         if (rep > 5) villageName = villageName + " that sees you as reputable";
         if (villager.isBaby()) {
-            entityName = "Villager Kid";
+            entityBaseName = "Villager Kid";
+            entityDisplayName = entityBaseName; // set display name to be the new base name
             return String.format("You see a kid in a %s. The kid shouts: \"", villageName);
         }
         String profession = StringUtils.capitalize(villager.getVillagerData().getProfession().toString().toLowerCase(Locale.ROOT).replace("none", "freelancer"));
-        entityName = profession;
-        if (villager.getVillagerData().getLevel() >= 3) profession = "skilled " + profession;
-        if (isHurt) profession = "hurt " + profession;
+        entityBaseName = profession; // overwrite base name with the profession
+        entityDisplayName = entityBaseName; // set display name to be the new base name
+        if (villager.getVillagerData().getLevel() >= 3) entityDisplayName = "skilled " + entityDisplayName; // modify display name
+        if (isHurt) entityDisplayName = "hurt " + entityDisplayName; // modify display name
         Text customName = villager.getCustomName();
         if (customName != null)
-            profession = profession + " called " + customName.getString();
-        return String.format("You meet a %s in a %s. It talks to you and says directly to you: \"", profession, villageName);
+            entityDisplayName = entityDisplayName + " called " + customName.getString(); // modify display name
+        return String.format("You meet a %s in a %s. It talks to you and says directly to you: \"", entityDisplayName, villageName);
     }
 
     public static String createPromptLiving(LivingEntity entity) {
         boolean isHurt = isEntityHurt(entity);
-        String baseName = entity.getName().getString();
-        String name = baseName;
+        entityBaseName = entity.getName().getString(); // set base name
+        entityDisplayName = entityBaseName; // initially set display name to be the base name
         Text customName = entity.getCustomName();
         if (customName != null)
-            name = baseName + " called " + customName.getString();
-        entityName = baseName;
-        if (isHurt) name = "hurt " + name;
-        return String.format("You meet a talking %s in the %s. The %s says to you: \"", name, getBiome(entity), baseName);
+            entityDisplayName = entityBaseName + " called " + customName.getString();  // modify display name
+        if (isHurt) entityDisplayName = "hurt " + entityDisplayName; // modify display name
+        return String.format("You meet a talking %s in the %s. The %s says to you: \"", entityDisplayName, getBiome(entity), entityBaseName);  // use base name here to keep the entity type in the conversation
     }
-
+    
     public static String createPrompt(Entity entity, PlayerEntity player) {
         if (entity instanceof VillagerEntity villager) return createPromptVillager(villager, player);
         if (entity instanceof LivingEntity entityLiving) return createPromptLiving(entityLiving);
-        entityName = entity.getName().getString();
-        return "You see a " + entityName + ". The " + entityName + " says: \"";
+        entityBaseName = entity.getName().getString();
+        entityDisplayName = entityBaseName; // initially set display name to be the base name
+        return "You see a " + entityDisplayName + ". The " + entityBaseName + " says: \"";
     }
 
     public static void handlePunch(Entity entity, Entity player) {
         if (entity.getId() != entityId) return;
-        prompts += ((player.getUuid() == initiator) ? "You punch" : (player.getName().getString() + " punches")) + " the " + entityName + ".\n";
+        prompts += "Suddenly, " + player.getName().getString() + " punches the " + entityDisplayName + ". The " + entityBaseName + " screams out in pain: \"";
+        getResponse((PlayerEntity) player);
     }
 }
